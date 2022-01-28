648ac3375091fdec1969afa6f8997354901b08c8
nightingaele <84741089+angelanadal@users.noreply.github.com>
2022-01-27 19:50:37 -0600

    Updated the api key

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const apiKey = "ce127c4a4bf4aa798458ca629a00045c";
+export const apiKey = "a8730d7b28118354d14e2046c817ba28";
 export const units = "metric";
 export function convertToFahrenheit(temp) {
   return (temp * 9) / 5 + 32;
